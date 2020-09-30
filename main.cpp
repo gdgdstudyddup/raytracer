645b3abf8035d109f2b7baee29840efb5b1fdf4b
dbbh <742520948@qq.com>
2020-09-30 09:23:28 +0800

    three spheres

--- main.cpp
+++ main.cpp
@@ -44,10 +44,12 @@ int main()
     vec3 vertical(0.0, 2.0, 0.0);
     vec3 origin(0.0, 0.0, 0.0);
     camera cam(vec3(-2, 1, 1), vec3(0, 0, -1), vec3(0, 1, 0), 90, float(nx) / float(ny));
-    hitable *list[2];
+    hitable *list[4];
     list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(1, 0, 0)));
-    list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0, 0, 1)));
-    hitable *world = new hitable_list(list, 2);
+    list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.5, 0.5, 0.5)));
+    list[2] = new sphere(vec3(1, 0, -1), 0.5, new lambertian(vec3(1, 1, 0)));
+    list[3] = new sphere(vec3(-1, 0, -1), 0.5, new lambertian(vec3(0, 0, 1)));
+    hitable *world = new hitable_list(list, 4);
     ofstream fout("result.ppm");
     if (!fout)
     {
